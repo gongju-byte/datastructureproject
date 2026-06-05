40e87eda654a0899d51f4ef51494e483d4818d17
gongju-byte <nurulizzatihayat@gmail.com>
2026-06-05 17:51:40 +0800

    commit

--- DSA_project/main.cpp
+++ DSA_project/main.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main() {
 	int input, sub;
+	BookStore bookstore;
+
 	while (true) {
 		string book[] = { "title1", "title2", "title3", "title4", "title5", "title6", "title7" };
 		//Bookstore books();
